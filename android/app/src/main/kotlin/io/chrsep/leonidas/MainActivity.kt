3ea941ba7b8459abdc51b72a9d9d779ba6af7c61
chrisando <chrisep8@gmail.com>
2019-08-19 13:18:40 +0700

    Add sentry on android framework.

--- android/app/src/main/kotlin/io/chrsep/leonidas/MainActivity.kt
+++ android/app/src/main/kotlin/io/chrsep/leonidas/MainActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.sentry.Sentry
+import io.sentry.android.AndroidSentryClientFactory
+import java.lang.Exception
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Sentry.init(
+                BuildConfig.SENTRYDSN,
+                AndroidSentryClientFactory(applicationContext)
+        )
+        GeneratedPluginRegistrant.registerWith(this)
+    }
 }
